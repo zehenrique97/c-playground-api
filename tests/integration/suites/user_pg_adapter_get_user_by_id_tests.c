3beacdbebfcc0d4cfa624750b3a54343c2bdcbf5
José Henrique Azevedo Secretário <jhsecretario@gmail.com>
2026-01-05 23:37:21 -0300

    tests: integration tests

--- tests/integration/suites/user_pg_adapter_get_user_by_id_tests.c
+++ tests/integration/suites/user_pg_adapter_get_user_by_id_tests.c
@@ -1,4 +1,5 @@
 #include <libpq-fe.h>
+#include <stdlib.h>
 #include "unity.h"
 #include "user.h"
 #include "user_pg_adapter_get_user_by_id_tests.h"
@@ -7,13 +8,14 @@
 
 void get_user_by_id_user_exists_return_user() {
     User expected_user = {.id = 1, .name = "User1", .age = 20};
-    User user;
+    User *user = calloc(1, sizeof(User));
     UserPgAdapter adapter = {.conn = conn};
-    UserRepoStatus rc = user_pg_adapter_get_user_by_id(&adapter, expected_user.id, &user);
+    UserRepoStatus rc = user_pg_adapter_get_user_by_id(&adapter, expected_user.id, user);
 
     TEST_ASSERT_EQUAL_INT(USER_REPO_SUCCESS, rc);
-    TEST_ASSERT_EQUAL_INT(expected_user.id, user.id);
-    TEST_ASSERT_EQUAL_INT(expected_user.age, user.age);
+    TEST_ASSERT_EQUAL_INT(expected_user.id, user->id);
+    TEST_ASSERT_EQUAL_CHAR_ARRAY(expected_user.name, user->name, sizeof(expected_user.name));
+    TEST_ASSERT_EQUAL_INT(expected_user.age, user->age);
 }
 
 void run_user_pg_adapter_get_user_by_id_tests() {
